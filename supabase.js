1d5544556266458a0481815e5c2c08887f8946e5
David Ndung'u Gitau <107082085+Astockorigins@users.noreply.github.com>
2026-08-10 22:12:03 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+    "https://qnfkmsgqzocbiuwmibgq.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_44l00aG3cAhS0P2iv34oWQ_BVi0OA8x";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
